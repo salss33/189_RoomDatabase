f9a154db8f77d9939f90e85b958344ad9ce8fe4a
Sal Sabila <hallosalsa03@gmail.com>
2025-11-28 21:02:12 +0700

    tujuan navigasi halaman utama, nyimpen route

---
+++ app/src/main/java/com/example/roomdatabase_189/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase_189.view.route
+
+import com.example.roomdatabase_189.R
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+
+}
