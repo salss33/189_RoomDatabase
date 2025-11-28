6314ebbc3a54670318ef8622aacb4abab613d0da
Sal Sabila <hallosalsa03@gmail.com>
2025-11-28 21:00:50 +0700

    definisiin tujuan halaman buat navigasi dalam aplikasi

---
+++ app/src/main/java/com/example/roomdatabase_189/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase_189.view.route
+
+import com.example.roomdatabase_189.R
+import com.example.roomdatabase_189.view.route.DestinasiNavigasi
+
+object DestinasiEntry: DestinasiNavigasi {
+    override val route = "item_entry"
+    override  val  titleRes = R.string.entry_siswa
+}
