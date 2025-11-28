7e0fdcd2a55193f19c3d3c25ab09c523b6f167f9
Sal Sabila <hallosalsa03@gmail.com>
2025-11-28 17:40:22 +0700

    @Database init daftar entitas versi database

--- app/src/main/java/com/example/roomdatabase_189/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_189/room/DatabaseSiswa.kt
@@ -7,21 +7,6 @@ import androidx.room.RoomDatabase
 import kotlinx.coroutines.selects.SelectInstance
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
-abstract class DatabaseSiswa : RoomDatabase(){
-    abstract  fun  siswaDao() : SiswaDao
+abstract class DatabaseSiswa{
 
-    companion object {
-        @Volatile
-        private var Instance: DatabaseSiswa? = null
-
-        fun getDatabase(context: Context): DatabaseSiswa {
-            return (Instance ?: synchronized(this) {
-                Room.databaseBuilder(
-                    context, klass = DatabaseSiswa::class.java,
-               "Siswa_database"
-                )
-                    .build().also { Instance = it }
-            })
-        }
-    }
 }
