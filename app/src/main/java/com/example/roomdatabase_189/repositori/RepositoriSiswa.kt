3228043e3835c58fce2d2355563ec272514e7f24
Sal Sabila <hallosalsa03@gmail.com>
2025-11-28 17:47:06 +0700

    lalu nanti diterusin perminataan ke siswaDao, buat ambil data siswa dan masukkin data baru biar lebih terstruktur dan rapi.

--- app/src/main/java/com/example/roomdatabase_189/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdatabase_189/repositori/RepositoriSiswa.kt
@@ -8,3 +8,9 @@ interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
 }
+    class OfflineRepositoriSiswa(
+        private val siswaDao: SiswaDao
+    ): RepositoriSiswa {
+    override fun  getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+}
